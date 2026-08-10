70e7e7dcaa05bdbc852dcf11aeec1d62cded2a8c
Anton <kornilov.anthon@yandex.ru>
2026-08-10 14:50:05 +0300

    fixed

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 
 int main()
 {
-    Matrix matrix(0);
+    Matrix<int, -1> matrix;
 
     for (int i = 0; i <= 9; ++i) matrix[i][i] = i;
 
@@ -15,7 +15,7 @@ int main()
         for (int c = 1; c <= 8; ++c)
         {
             if (c != 1) std::cout << ' ';
-            std::cout << matrix(r, c);
+            std::cout << matrix[r][c];
         }
         std::cout << '\n';
     }
--- src/matrix.h
+++ src/matrix.h
@@ -5,10 +5,11 @@
 #include <unordered_map>
 #include <tuple>
 
+template <typename T, T DefaultValue>
 class Matrix
 {
 public:
-    explicit Matrix(int defaultValue = 0) : m_default(defaultValue) {}
+    explicit Matrix(T defaultValue = DefaultValue) : m_default(defaultValue) {}
 
     static inline long long keyOf(int r, int c) noexcept {
         return (static_cast<long long>(r) << 32) | static_cast<unsigned int>(c);
@@ -25,21 +26,21 @@ public:
     };
 
     RowProxy operator[](size_t r) { return RowProxy(*this, static_cast<int>(r)); }
-    int operator()(size_t r, size_t c) const { return get(static_cast<int>(r), static_cast<int>(c)); }
+    T operator()(size_t r, size_t c) const { return get(static_cast<int>(r), static_cast<int>(c)); }
 
     std::size_t size() const noexcept { return m_store.size(); }
 
     class iterator {
     public:
-        using inner_it = std::unordered_map<long long,int>::const_iterator;
+        using inner_it = typename std::unordered_map<long long, T>::const_iterator;
         iterator(inner_it it) : it_(it) {}
         iterator& operator++() { ++it_; return *this; }
         bool operator!=(iterator const& o) const { return it_ != o.it_; }
-        std::tuple<int,int,int> operator*() const {
+        std::tuple<int,int,T> operator*() const {
             long long k = it_->first;
             int r = static_cast<int>(k >> 32);
             int c = static_cast<int>(k & 0xffffffff);
-            return {r, c, it_->second};
+            return std::make_tuple(r, c, it_->second);
         }
     private:
         inner_it it_;
@@ -48,31 +49,34 @@ public:
     iterator end() const { return iterator(m_store.cend()); }
 
 private:
-    int get(int r, int c) const {
+    T get(int r, int c) const {
         auto it = m_store.find(keyOf(r,c));
         if (it == m_store.end()) return m_default;
         return it->second;
     }
-    void set(int r, int c, int v) {
+    void set(int r, int c, T v) {
         auto k = keyOf(r,c);
         if (v == m_default) m_store.erase(k);
         else m_store[k] = v;
     }
 
-    int m_default{0};
-    std::unordered_map<long long,int> m_store;
+    T m_default{DefaultValue};
+    std::unordered_map<long long,T> m_store;
 
 public:
     // Cell proxy allows `matrix[row][col] = val` and reading as `int`
     class CellProxy {
     public:
         CellProxy(Matrix& m, int r, int c) : m_(m), r_(r), c_(c) {}
-        CellProxy& operator=(int v) { m_.set(r_, c_, v); return *this; }
-        operator int() const { return m_.get(r_, c_); }
+        CellProxy& operator=(T v) { m_.set(r_, c_, v); return *this; }
+        operator T() const { return m_.get(r_, c_); }
     private:
         Matrix& m_;
         int r_, c_;
     };
 };
 
-inline Matrix::CellProxy Matrix::RowProxy::operator[](size_t c) { return CellProxy(m_, r_, static_cast<int>(c)); }
+template <typename T, T DefaultValue>
+inline typename Matrix<T, DefaultValue>::CellProxy Matrix<T, DefaultValue>::RowProxy::operator[](size_t c) {
+    return CellProxy(m_, r_, static_cast<int>(c));
+}
